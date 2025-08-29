9aa138225a114f080babbfad0ceafa94efbd14ad
Alexandre Andrioli Tucci <alexandreatucci@gmail.com>
2025-08-28 21:41:59 -0300

    nascimento

--- src/main/java/com/buks/buks/dto/UsuarioDTO.java
+++ src/main/java/com/buks/buks/dto/UsuarioDTO.java
@@ -3,8 +3,11 @@ package com.buks.buks.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
@@ -16,9 +19,14 @@ public class UsuarioDTO {
     @NotBlank(message = "O nome precisa ser preenchido.")
     private String nome;
 
-    @Email
-    @NotBlank
+    @Email(message = "Email inválido")
+    @NotBlank(message = "O email precisa ser preenchido.")
     private String email;
 
+    @NotBlank(message = "A senha precisa ser preenchida.")
+    private String senha;
 
+    @NotNull(message = "A data de nascimento precisa ser preenchida.")
+    @Past(message = "A data de nascimento deve ser no passado.")
+    private LocalDate dataNascimento;
 }
