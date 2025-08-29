095ca5386137fa096c9e6f10f20fb5474e74e3d3
Alexandre Andrioli Tucci <alexandreatucci@gmail.com>
2025-08-28 21:31:11 -0300

    parto

---
+++ src/main/java/com/buks/buks/BuksApplication.java
@@ -0,0 +1,13 @@
+package com.buks.buks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuksApplication.class, args);
+	}
+
+}
